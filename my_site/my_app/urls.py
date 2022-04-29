73d430a6cf6d405f223ca592a943487be24a9653
Ty <usmc.millert@gmail.com>
2022-04-28 22:34:01 -0400

    section 9 wrap up

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.simple_view)
+    path('<int:num_page>', views.num_page_view),
+    path('<str:topic>/', views.news_view),
 ]
